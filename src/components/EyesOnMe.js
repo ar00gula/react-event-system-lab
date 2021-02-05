c3c2f4ccfed914b762dd4c0f633f37df1cf6e0ff
ar00gula <arianna.mayer@gmail.com>
2021-02-05 13:55:36 -0500

    commit tree 87210b1aecc0cc677d934257825b161c7ff6544a

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,7 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    render() {
+        return <button onFocus={(event) => console.log('Good!')} onBlur={(event) => console.log('Hey! Eyes on me!')}></button>
+    }
+}
